1210152c33f15f9b57329793f07a9949bb0be549
tahsan2 <96606113+tahsan2@users.noreply.github.com>
2024-03-28 10:33:14 +0600

    first commit

---
+++ app/Models/Product.php
@@ -0,0 +1,23 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+
+class Product extends Model
+{
+    use HasFactory;
+    protected $guarded  = ['id'];
+
+    function rel_to_cat(){
+        return $this->belongsTo(Category::class, 'category_id');
+    }
+    function rel_to_tag(){
+        return $this->belongsTo(Tag::class, 'tags');
+    }
+    function rel_to_inventory(){
+        return $this->hasMany(Inventory::class, 'product_id');
+    }
+}
+
---
+++ routes/api.php
@@ -0,0 +1,39 @@
+<?php
+
+use App\Http\Controllers\API\CategoryApiController;
+use App\Http\Controllers\API\CustomerAuthentication;
+use Illuminate\Http\Request;
+use Illuminate\Support\Facades\Route;
+
+/*
+|--------------------------------------------------------------------------
+| API Routes
+|--------------------------------------------------------------------------
+|
+| Here is where you can register API routes for your application. These
+| routes are loaded by the RouteServiceProvider and all of them will
+| be assigned to the "api" middleware group. Make something great!
+|
+*/
+
+// Route::middleware('auth:sanctum')->get('/user', function (Request $request) {
+//     return $request->user();
+// });
+
+Route::middleware('auth:sanctum')->group( function () {
+    Route::post('/category/store', [CategoryApiController::class, 'category_store']);
+    Route::get('/category/show/{id}', [CategoryApiController::class, 'category_show']);
+    Route::post('/category/update/{id}', [CategoryApiController::class, 'category_update']);
+    Route::delete('/category/delete/{id}', [CategoryApiController::class, 'category_delete']);
+});
+
+//customer authentication]
+Route::post('/customer/register', [CustomerAuthentication::class, 'customer_register']);
+Route::post('/customer/login', [CustomerAuthentication::class, 'customer_login']);
+Route::post('/customer/logout', [CustomerAuthentication::class, 'customer_logout']);
+
+
+
+//category
+Route::get('/get/category', [CategoryApiController::class, 'get_category']);
+
